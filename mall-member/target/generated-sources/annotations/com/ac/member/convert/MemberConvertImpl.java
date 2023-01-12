08a6d14ce0a718dc8c7a9ba900c850f6c43b3e1d
chenyan <chenyan@xiangxiang.cn>
2023-01-12 09:39:14 +0800

    gitignore

--- mall-member/target/generated-sources/annotations/com/ac/member/convert/MemberConvertImpl.java
+++ mall-member/target/generated-sources/annotations/com/ac/member/convert/MemberConvertImpl.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-01-11T17:13:26+0800",
+    date = "2023-01-11T17:46:18+0800",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 1.8.0_131 (Oracle Corporation)"
 )
 public class MemberConvertImpl implements MemberConvert {
