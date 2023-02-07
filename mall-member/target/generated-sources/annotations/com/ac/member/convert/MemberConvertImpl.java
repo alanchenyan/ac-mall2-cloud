c03eb63c1ddd459e90497b8c2faebb8f3bfbb265
chenyan <chenyan@xiangxiang.cn>
2023-02-07 16:37:43 +0800

    Nacos

--- mall-member/target/generated-sources/annotations/com/ac/member/convert/MemberConvertImpl.java
+++ mall-member/target/generated-sources/annotations/com/ac/member/convert/MemberConvertImpl.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-02-06T15:14:08+0800",
+    date = "2023-02-07T15:56:51+0800",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 1.8.0_131 (Oracle Corporation)"
 )
 public class MemberConvertImpl implements MemberConvert {
