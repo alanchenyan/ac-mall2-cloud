5489ab4c784b25667686e7fe82e2f7b1eb478262
chenyan <chenyan@xiangxiang.cn>
2023-02-21 17:06:21 +0800

    Update OrderController.java

--- mall-order/src/main/java/com/ac/order/controller/OrderController.java
+++ mall-order/src/main/java/com/ac/order/controller/OrderController.java
@@ -1,9 +1,10 @@
 package com.ac.order.controller;
 
-import com.ac.feign.member.api.MemberFeignApi;
-import com.ac.feign.member.dto.MemberDTO;
+import com.ac.order.dto.OrderDTO;
+import com.ac.order.qry.OrderPageQry;
 import com.ac.order.service.OrderService;
 import com.ac.order.vo.OrderAddVO;
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
@@ -17,21 +18,17 @@ import javax.validation.Valid;
 public class OrderController {
 
     @Resource
-    private MemberFeignApi memberFeignApi;
-
-    @Resource
     private OrderService orderServiceImpl;
-
-    @ApiOperation(value = "通过OpenFeign取用户数据")
-    @GetMapping("feign/member/{id}")
-    public MemberDTO test(@PathVariable Long id) {
-        return memberFeignApi.findMember(id);
-    }
-
 
     @ApiOperation(value = "新增订单")
     @PostMapping
     public Long createOrder(@RequestBody @Valid OrderAddVO addVO) {
         return orderServiceImpl.createOrder(addVO);
+    }
+
+    @ApiOperation(value = "分页查询订单")
+    @GetMapping("page")
+    public IPage<OrderDTO> pageOrder(@Valid OrderPageQry qry) {
+        return orderServiceImpl.pageOrder(qry);
     }
 }
