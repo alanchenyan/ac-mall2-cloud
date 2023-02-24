92e20bfbc2a800e31361d6a946fbd311ecadcc86
chenyan <chenyan@xiangxiang.cn>
2023-02-24 17:12:21 +0800

    redis

--- mall-member/src/main/java/com/ac/member/controller/RedisTestController.java
+++ mall-member/src/main/java/com/ac/member/controller/RedisTestController.java
@@ -1,5 +1,6 @@
 package com.ac.member.controller;
 
+import com.ac.common.util.RedisComponent;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -13,14 +14,14 @@ import javax.annotation.Resource;
 public class RedisTestController {
 
     @Resource
-    private RedisTemplate redisTemplate;
+    private RedisComponent redisComponent;
 
     @ApiOperation(value = "存-取-字符串")
     @GetMapping("testString")
-    public String testString(@RequestParam String value) {
+    public String testString2(@RequestParam String value) {
         String key = "memberName";
-        redisTemplate.opsForValue().set(key, value);
-        Object result = redisTemplate.opsForValue().get(key);
+        redisComponent.set(key,value,0);
+        Object result = redisComponent.get(key);
         if (result != null) {
             return result.toString();
         }
