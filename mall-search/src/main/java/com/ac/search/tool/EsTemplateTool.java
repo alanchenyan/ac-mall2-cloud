a736e452b12d77c2a58930d92aad2098bedbab8f
chenyan <chenyan@xiangxiang.cn>
2023-04-12 17:58:07 +0800

    Update EsTemplateTool.java

--- mall-search/src/main/java/com/ac/search/tool/EsTemplateTool.java
+++ mall-search/src/main/java/com/ac/search/tool/EsTemplateTool.java
@@ -3,6 +3,7 @@ package com.ac.search.tool;
 import cn.hutool.core.bean.BeanUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+import org.springframework.data.elasticsearch.core.document.Document;
 import org.springframework.data.elasticsearch.core.mapping.IndexCoordinates;
 import org.springframework.data.elasticsearch.core.query.UpdateQuery;
 import org.springframework.stereotype.Component;
@@ -35,8 +36,11 @@ public class EsTemplateTool {
     }
 
     public void updateDoc(String indexName, String docId, Object docObj) {
+        Document document = Document.create();
         Map<String, Object> params = BeanUtil.beanToMap(docObj);
-        UpdateQuery query = UpdateQuery.builder(docId).withParams(params).build();
+        document.putAll(params);
+
+        UpdateQuery query = UpdateQuery.builder(docId).withDocument(document).build();
         elasticsearchRestTemplate.update(query, IndexCoordinates.of(indexName));
     }
 }
