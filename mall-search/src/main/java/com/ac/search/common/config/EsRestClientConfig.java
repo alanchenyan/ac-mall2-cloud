a09c80dc2f0f84169855c2e7e99e875525b0dfaf
chenyan <chenyan@xiangxiang.cn>
2023-04-14 10:21:00 +0800

    Update EsRestClientConfig.java

--- mall-search/src/main/java/com/ac/search/common/config/EsRestClientConfig.java
+++ mall-search/src/main/java/com/ac/search/common/config/EsRestClientConfig.java
@@ -1,6 +1,7 @@
 package com.ac.search.common.config;
 
 import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
@@ -12,11 +13,15 @@ import org.springframework.data.elasticsearch.repository.config.EnableElasticsea
 @EnableElasticsearchRepositories
 public class EsRestClientConfig extends AbstractElasticsearchConfiguration {
 
+    @Value("${es.url}")
+    private String esUrl;
+
     @Override
     @Bean
     public RestHighLevelClient elasticsearchClient() {
+        System.out.println(esUrl);
         final ClientConfiguration clientConfiguration = ClientConfiguration.builder()
-                .connectedTo("127.0.0.1:9200")
+                .connectedTo(esUrl)
                 .build();
 
         return RestClients.create(clientConfiguration).rest();
