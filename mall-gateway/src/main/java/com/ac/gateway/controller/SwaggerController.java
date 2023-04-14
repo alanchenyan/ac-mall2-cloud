695252008971e4541f80b67a8c4b2fcdb20b6a02
chenyan <chenyan@xiangxiang.cn>
2023-04-14 18:37:58 +0800

    Update SwaggerController.java

--- mall-gateway/src/main/java/com/ac/gateway/controller/SwaggerController.java
+++ mall-gateway/src/main/java/com/ac/gateway/controller/SwaggerController.java
@@ -1,42 +1,26 @@
 package com.ac.gateway.controller;
 
 import com.ac.gateway.config.SwaggerConfig;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
+import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger.web.UiConfigurationBuilder;
 
 import javax.annotation.Resource;
 
 @RestController
 public class SwaggerController {
-    //@Resource
-    //private SecurityConfiguration securityConfiguration;
-
-//    @Resource
-//    private UiConfiguration uiConfiguration;
 
     @Resource
     private SwaggerConfig swaggerConfig;
 
-    @Autowired
-    public SwaggerController(SwaggerConfig swaggerConfig) {
-        this.swaggerConfig = swaggerConfig;
+    @GetMapping("/swagger-resources/configuration/ui")
+    public Mono<ResponseEntity<UiConfiguration>> uiConfiguration() {
+        return Mono.just(new ResponseEntity<>(UiConfigurationBuilder.builder().build(), HttpStatus.OK));
     }
-
-//    @GetMapping("/swagger-resources/configuration/security")
-//    public Mono<ResponseEntity<SecurityConfiguration>> securityConfiguration() {
-//        return Mono.just(new ResponseEntity<>(
-//                Optional.ofNullable(securityConfiguration).orElse(SecurityConfigurationBuilder.builder().build()), HttpStatus.OK));
-//    }
-
-//    @GetMapping("/swagger-resources/configuration/ui")
-//    public Mono<ResponseEntity<UiConfiguration>> uiConfiguration() {
-//        return Mono.just(new ResponseEntity<>(
-//                Optional.ofNullable(uiConfiguration).orElse(UiConfigurationBuilder.builder().build()), HttpStatus.OK));
-//    }
 
     @GetMapping("/swagger-resources")
     public Mono<ResponseEntity> swaggerResources() {
