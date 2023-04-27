8b35cf6f960841810e8151d50636e93bad51aa79
chenyan <chenyan@xiangxiang.cn>
2023-04-27 21:21:17 +0800

    Update SwaggerController.java

--- mall-gateway/src/main/java/com/ac/gateway/controller/SwaggerController.java
+++ mall-gateway/src/main/java/com/ac/gateway/controller/SwaggerController.java
@@ -1,7 +1,6 @@
 package com.ac.gateway.controller;
 
 import com.ac.gateway.config.GateWaySwaggerConfig;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,7 +12,6 @@ import springfox.documentation.swagger.web.UiConfiguration;
 import springfox.documentation.swagger.web.UiConfigurationBuilder;
 
 import javax.annotation.Resource;
-import java.util.Optional;
 
 @RestController
 public class SwaggerController {
@@ -21,22 +19,14 @@ public class SwaggerController {
     @Resource
     private GateWaySwaggerConfig gateWaySwaggerConfig;
 
-    @Autowired(required = false)
-    private SecurityConfiguration securityConfiguration;
-
-    @Autowired(required = false)
-    private UiConfiguration uiConfiguration;
-
     @GetMapping("/swagger-resources/configuration/security")
     public Mono<ResponseEntity<SecurityConfiguration>> securityConfiguration() {
-        return Mono.just(new ResponseEntity<>(
-                Optional.ofNullable(securityConfiguration).orElse(SecurityConfigurationBuilder.builder().build()), HttpStatus.OK));
+        return Mono.just(new ResponseEntity<>(SecurityConfigurationBuilder.builder().build(), HttpStatus.OK));
     }
 
     @GetMapping("/swagger-resources/configuration/ui")
     public Mono<ResponseEntity<UiConfiguration>> uiConfiguration() {
-        return Mono.just(new ResponseEntity<>(
-                Optional.ofNullable(uiConfiguration).orElse(UiConfigurationBuilder.builder().build()), HttpStatus.OK));
+        return Mono.just(new ResponseEntity<>(UiConfigurationBuilder.builder().build(), HttpStatus.OK));
     }
 
     @GetMapping("/swagger-resources")
