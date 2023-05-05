782cad2c5294a9fa8bd1fa97a2ba756384b77588
chenyan <chenyan@xiangxiang.cn>
2023-05-05 15:28:40 +0800

    Update t_member.sql

--- mall-member/src/main/resources/sql/t_member.sql
+++ mall-member/src/main/resources/sql/t_member.sql
@@ -1,12 +1,12 @@
 CREATE TABLE `t_member` (
   `id` bigint NOT NULL COMMENT 'id',
   `member_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT '' COMMENT '用户姓名',
-  `id_no` char(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT '' COMMENT '用户手机号码',
+  `mobile` char(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT '' COMMENT '用户手机号码',
   `sex` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT 'UNKNOW' COMMENT '用户性别',
   `birthday` date DEFAULT NULL COMMENT '生日',
   `deleted` bit(1) NOT NULL DEFAULT b'0' COMMENT '是否删除 0未删除 1已删除',
   `update_time` datetime NOT NULL COMMENT '修改时间',
   `create_time` datetime NOT NULL COMMENT '创建时间',
   PRIMARY KEY (`id`) USING BTREE,
-  KEY `IDX_mobile` (`idNo`)
+  KEY `IDX_mobile` (`mobile`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC COMMENT='用户'
